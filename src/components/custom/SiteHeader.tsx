bab6300d5f786d1da84a93d2ee2145bb3ffd3aed
santhosh <santhosh.003z@gmail.com>
2025-12-06 14:06:42 +0530

    update site header with props

--- src/components/custom/SiteHeader.tsx
+++ src/components/custom/SiteHeader.tsx
@@ -1,17 +1,26 @@
-import getCurrentDate from "@/utils/getCurrentDate.ts";
+import { getCurrentDate, formatLoginDate } from "@/utils/formatDate";
 
-function SiteHeader() {
-  const currentDate: String = getCurrentDate();
+function SiteHeader({
+  heading,
+  name = null,
+  lastLogin,
+}: {
+  heading: string;
+  name: string | null;
+  lastLogin: string;
+}) {
+  const currentDate: string = getCurrentDate();
+  const lastLoginFormatted: string = formatLoginDate(lastLogin);
 
   return (
     <header className="w-full flex">
       <div className="flex-auto flex flex-col text-3xl">
-        <h1>Welcome Back,</h1>
-        <h2>Santa</h2>
+        <h1>{heading}</h1>
+        {name && <h2>name</h2>}
       </div>
       <div className=" flex flex-col items-center gap-2">
         <h2 className="text-4xl">{currentDate}</h2>
-        <h2 className="text-sm">last login Dec 1, 2025</h2>
+        <h2 className="text-sm">last login {lastLoginFormatted}</h2>
       </div>
     </header>
   );
