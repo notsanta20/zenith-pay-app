5e5480b781fabb2f5e27109bf498935c990508de
santhosh <santhosh.003z@gmail.com>
2025-12-06 13:51:56 +0530

    added outlet for nested elements

--- src/app/routes/app/Home.tsx
+++ src/app/routes/app/Home.tsx
@@ -1,14 +1,12 @@
 import AppSidebar from "@/components/custom/AppSidebar";
-import SiteHeader from "@/components/custom/SiteHeader";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { Outlet } from "react-router";
 
 function Home() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset className="px-5 py-5 flex flex-col">
-        <SiteHeader />
-      </SidebarInset>
+      <Outlet />
     </SidebarProvider>
   );
 }
