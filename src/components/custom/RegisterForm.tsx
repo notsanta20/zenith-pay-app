0fabeaec2be8284e525d95af2e3d3c8635f1f1f6
santhosh <santhosh.003z@gmail.com>
2025-12-09 16:20:37 +0530

    add register form component

---
+++ src/components/custom/RegisterForm.tsx
@@ -0,0 +1,168 @@
+import { useForm } from "@tanstack/react-form";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { registerFormSchema } from "@/schemas/formSchemas";
+import { toast } from "sonner";
+import type { registerFormRequest } from "@/types/types";
+import { registerApi } from "@/apis/postRequests";
+import type { AxiosError, AxiosResponse } from "axios";
+import { Spinner } from "../ui/spinner";
+
+export function RegisterForm() {
+  const navigate = useNavigate();
+  const register = useMutation({
+    mutationKey: ["register"],
+    mutationFn: (data: registerFormRequest) => {
+      return registerApi(data);
+    },
+    onError: (error: AxiosError) => {
+      toast.error(error.response?.data.message);
+    },
+    onSuccess: (data: AxiosResponse) => {
+      toast.success(data.data.message);
+
+      navigate("/login", { replace: true });
+    },
+  });
+
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validators: {
+      onSubmit: registerFormSchema,
+    },
+    onSubmit: async ({ value }) => {
+      const data: registerFormRequest = {
+        email: value.email,
+        password: value.password,
+        isAdmin: false,
+      };
+
+      register.mutate(data);
+    },
+  });
+
+  return (
+    <Card className="lg:min-w-[450px]">
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Create Account</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form
+          id="register-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            form.handleSubmit();
+          }}
+        >
+          <FieldGroup>
+            <form.Field
+              name="email"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid;
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                    <Input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      aria-invalid={isInvalid}
+                      placeholder="z@example.com"
+                      autoComplete="off"
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                );
+              }}
+            />
+            <form.Field
+              name="password"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid;
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                    <Input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      type="password"
+                      placeholder="z#Ez3tkDr#$5wj2s*1j&"
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                );
+              }}
+            />
+            <form.Field
+              name="confirmPassword"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid;
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>
+                      Confirm Password
+                    </FieldLabel>
+                    <Input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      type="password"
+                      placeholder="z#Ez3tkDr#$5wj2s*1j&"
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                );
+              }}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Field>
+          <Button type="submit" form="register-form">
+            {register.isPending ? <Spinner /> : "Sign up"}
+          </Button>
+          <FieldDescription className="text-center">
+            Don&apos;t have an account? <a href="/login">login</a>
+          </FieldDescription>
+        </Field>
+      </CardFooter>
+    </Card>
+  );
+}
