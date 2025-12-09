8d843f8ee738e383f4f9620ec27398dfdd80cbcf
santhosh <santhosh.003z@gmail.com>
2025-12-09 16:17:09 +0530

    added tanstack query provider and toaster

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./app/App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/sonner";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
