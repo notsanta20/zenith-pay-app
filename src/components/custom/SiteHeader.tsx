2039ffc2600a1f6432739990cb4d2fd13cf79fa8
santhosh <santhosh.003z@gmail.com>
2025-12-09 16:25:55 +0530

    add theme toggle and adjusted date text size

--- src/components/custom/SiteHeader.tsx
+++ src/components/custom/SiteHeader.tsx
@@ -1,26 +1,28 @@
 import { getCurrentDate, formatLoginDate } from "@/utils/formatDate";
+import { ModeToggle } from "../ModeToggle";
 
 function SiteHeader({
   heading,
   name = null,
-  lastLogin,
 }: {
   heading: string;
   name: string | null;
-  lastLogin: string;
 }) {
   const currentDate: string = getCurrentDate();
-  const lastLoginFormatted: string = formatLoginDate(lastLogin);
+  const lastLoginFormatted: string = formatLoginDate("2025-12-02 06:55:26.957");
 
   return (
     <header className="w-full flex">
-      <div className="flex-auto flex flex-col text-3xl">
+      <div className="flex-auto flex flex-col text-3xl font-semibold">
         <h1>{heading}</h1>
-        {name && <h2>name</h2>}
+        {name && <h2>{name}</h2>}
       </div>
-      <div className=" flex flex-col items-center gap-2">
-        <h2 className="text-4xl">{currentDate}</h2>
-        <h2 className="text-sm">last login {lastLoginFormatted}</h2>
+      <div className="flex items-center gap-3">
+        <ModeToggle />
+        <div className=" flex flex-col items-center">
+          <h2 className="text-3xl font-semibold">{currentDate}</h2>
+          <h2 className="text-sm">last login {lastLoginFormatted}</h2>
+        </div>
       </div>
     </header>
   );
