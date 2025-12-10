a33caa97b722806751d2de1844c7f5a494f43f03
santhosh <santhosh.003z@gmail.com>
2025-12-11 00:40:30 +0530

    move balance, accounts and transactions to separate component

--- src/app/routes/app/Dashboard.tsx
+++ src/app/routes/app/Dashboard.tsx
@@ -1,67 +1,17 @@
-import { Card } from "@/components/custom/Card";
-import LatestTransactions from "@/components/custom/LatestTransactions";
-import SimpleHeading from "@/components/custom/SimpleHeading";
+import Balance from "@/components/custom/Dashboard/Balance";
 import SiteHeader from "@/components/custom/SiteHeader";
 import { SidebarInset } from "@/components/ui/sidebar";
+import Accounts from "@/components/custom/Dashboard/Accounts";
+import Transactions from "@/components/custom/Dashboard/Transactions";
 
 function Dashboard() {
   return (
     <SidebarInset className="px-5 py-5 flex flex-col min-w-0 hover:cursor-default">
       <SiteHeader heading="Welcome back," name="santa" />
       <main className="flex-auto flex flex-col py-25 gap-30">
-        <div className="flex flex-col gap-5">
-          <SimpleHeading text="Balance" />
-          <div>
-            <span
-              className="font-(family-name:--dm-serif) text-6xl"
-              onMouseOver={(e) => {
-                e.target.textContent = "₹12,28,843.41";
-              }}
-              onMouseOut={(e) => {
-                e.target.textContent = "(╥﹏╥)";
-              }}
-            >
-              (⌐■_■)
-            </span>
-          </div>
-        </div>
-        <div className="flex flex-col gap-5">
-          <SimpleHeading text="Accounts" />
-          <div className="flex gap-5 overflow-auto">
-            <Card
-              variant="default"
-              accountName="Savings"
-              balance={98298.3}
-              outstanding={21701.7}
-              accountNumber="3600"
-            />
-            <Card
-              variant="orange"
-              accountName="Credit card"
-              balance={98298.3}
-              outstanding={21701.7}
-              accountNumber="3600"
-            />
-            <Card
-              variant="blue"
-              accountName="Savings"
-              balance={98298.3}
-              outstanding={21701.7}
-              accountNumber="3600"
-            />
-            <Card
-              variant="purple"
-              accountName="Savings"
-              balance={98298.3}
-              outstanding={21701.7}
-              accountNumber="3600"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col gap-5">
-          <SimpleHeading text="Latest transactions" />
-          <LatestTransactions />
-        </div>
+        <Balance />
+        <Accounts />
+        <Transactions />
       </main>
     </SidebarInset>
   );
