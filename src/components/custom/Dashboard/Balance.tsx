fc5a93d5e881642f853292675881c1d23f4c873d
santhosh <santhosh.003z@gmail.com>
2025-12-11 00:41:18 +0530

    add balance component with balance query fetch

---
+++ src/components/custom/Dashboard/Balance.tsx
@@ -0,0 +1,56 @@
+import { useContext, type MouseEvent } from "react";
+import SimpleHeading from "../SimpleHeading";
+import type { userIdContext } from "@/types/types";
+import { UserIdContext } from "@/context/context";
+import { useQuery } from "@tanstack/react-query";
+import { getTotalBalance } from "@/apis/getRequests";
+import { toast } from "sonner";
+import { formatCurrency } from "@/utils/formatCurrency";
+
+export default function Balance() {
+  const userId: userIdContext | undefined = useContext(UserIdContext);
+
+  const totalBalanceQuery = useQuery({
+    queryKey: ["total-balance"],
+    queryFn: async () => {
+      if (userId) {
+        const data = await getTotalBalance(
+          "43accbfc-8922-4b40-9151-cf750feab67d",
+        );
+        return data;
+      }
+    },
+    retry: false,
+  });
+
+  if (totalBalanceQuery.isError) {
+    if (totalBalanceQuery.error.status === 404) {
+      toast.error("No accounts available, create a account");
+    }
+  }
+
+  const balance: string = totalBalanceQuery.isSuccess
+    ? formatCurrency(totalBalanceQuery.data.data)
+    : "(⌐■_■)";
+
+  return (
+    <section className="flex flex-col gap-5">
+      <SimpleHeading text="Balance" />
+      <div>
+        <span
+          className="font-(family-name:--dm-serif) text-6xl"
+          onMouseOver={(e: MouseEvent) => {
+            const targetElement: HTMLSpanElement = e.target as HTMLSpanElement;
+            targetElement.textContent = balance;
+          }}
+          onMouseOut={(e: MouseEvent) => {
+            const targetElement: HTMLSpanElement = e.target as HTMLSpanElement;
+            targetElement.textContent = "(╥﹏╥)";
+          }}
+        >
+          (⌐■_■)
+        </span>
+      </div>
+    </section>
+  );
+}
