ffedd9304234164ce847c3c5fee8add8588640a7
santhosh <santhosh.003z@gmail.com>
2025-12-11 00:38:35 +0530

    add total balance and all accounts api

--- src/apis/getRequests.ts
+++ src/apis/getRequests.ts
@@ -2,9 +2,24 @@ import axios from "axios";
 
 const authURL: string = "http://localhost:8081/auth";
 const profileURL: string = "http://localhost:8082/profile";
+const accountURL: string = "http://localhost:8083/accounts";
 
 export async function getUserProfile(userId: string) {
   const data = await axios.get(profileURL + "/me/" + userId);
+
+  return data;
+}
+
+export async function getTotalBalance(userId: string) {
+  const data = await axios.get(
+    accountURL + "/user/" + userId + "/total-balance",
+  );
+
+  return data;
+}
+
+export async function getAllAccounst(userId: string) {
+  const data = await axios.get(accountURL + "/user/" + userId);
 
   return data;
 }
