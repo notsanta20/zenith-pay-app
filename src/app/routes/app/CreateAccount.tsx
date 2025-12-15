856f51d0e057c2fbc2e828083707af691fd2730e
santhosh <santhosh.003z@gmail.com>
2025-12-16 01:14:07 +0530

    add create account component

---
+++ src/app/routes/app/CreateAccount.tsx
@@ -0,0 +1,15 @@
+import { AccountForm } from "@/components/custom/AccountForm";
+
+export default function CreateAccount() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex flex-col gap-6 items-center w-full max-w-sm ">
+        <div className="flex items-center gap-2 font-medium">
+          <img src="./src/assets/icons/infinity.svg" alt="infinity logo" />
+          Zenith
+        </div>
+        <AccountForm />
+      </div>
+    </div>
+  );
+}
