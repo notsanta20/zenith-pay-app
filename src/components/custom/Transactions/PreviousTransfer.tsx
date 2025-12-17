42fbf7941b9cc7f64191f7382cda5d57e70f840b
santhosh <santhosh.003z@gmail.com>
2025-12-17 19:48:37 +0530

    add previous transactions

---
+++ src/components/custom/Transactions/PreviousTransfer.tsx
@@ -0,0 +1,7 @@
+export default function PreviousTransfers() {
+  return (
+    <section className="flex flex-col gap-5 bg-sidebar rounded-lg p-5">
+      <h2 className="font-semibold text-lg">Previous Transfers</h2>
+    </section>
+  );
+}
