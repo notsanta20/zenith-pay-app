1cd5cd01a5bac961a181b666c6585045e551d9ec
santhosh <santhosh.003z@gmail.com>
2025-12-17 19:48:05 +0530

    rename prop, added last login from auth context

--- src/components/custom/SiteHeader.tsx
+++ src/components/custom/SiteHeader.tsx
@@ -1,25 +1,30 @@
 import { getCurrentDate, formatLoginDate } from "@/utils/formatDate";
 import { ModeToggle } from "../ModeToggle";
+import type { AuthContextValue } from "@/types/types";
+import { useAuth } from "@/context/AuthContext";
 
 function SiteHeader({
   heading,
-  name = null,
+  username,
 }: {
   heading: string;
-  name: string | null;
+  username: string | null;
 }) {
+  const { lastLogin }: AuthContextValue = useAuth();
   const currentDate: string = getCurrentDate();
-  const lastLoginFormatted: string = formatLoginDate("2025-12-02 06:55:26.957");
+  const lastLoginFormatted: string = lastLogin
+    ? formatLoginDate(lastLogin)
+    : "null";
 
   return (
     <header className="w-full flex">
       <div className="flex-auto flex flex-col text-3xl font-semibold">
         <h1>{heading}</h1>
-        {name && <h2>{name}</h2>}
+        {username && <h2>{username}</h2>}
       </div>
       <div className="flex items-center gap-3">
         <ModeToggle />
-        <div className=" flex flex-col items-center">
+        <div className=" flex flex-col items-end">
           <h2 className="text-3xl font-semibold">{currentDate}</h2>
           <h2 className="text-sm">last login {lastLoginFormatted}</h2>
         </div>
