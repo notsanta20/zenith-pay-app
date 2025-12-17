e04fb9780e61648a3e330bd5d1bfe06ebfd5c411
santhosh <santhosh.003z@gmail.com>
2025-12-17 19:43:00 +0530

    rename header prop

--- src/app/routes/app/Dashboard.tsx
+++ src/app/routes/app/Dashboard.tsx
@@ -3,11 +3,15 @@ import SiteHeader from "@/components/custom/SiteHeader";
 import { SidebarInset } from "@/components/ui/sidebar";
 import Accounts from "@/components/custom/Dashboard/Accounts";
 import Transactions from "@/components/custom/Dashboard/Transactions";
+import type { AuthContextValue } from "@/types/types";
+import { useAuth } from "@/context/AuthContext";
 
 function Dashboard() {
+  const { username }: AuthContextValue = useAuth();
+
   return (
     <SidebarInset className="px-5 py-5 flex flex-col min-w-0 hover:cursor-default">
-      <SiteHeader heading="Welcome back," name="santa" />
+      <SiteHeader heading="Welcome back," username={username} />
       <main className="flex-auto flex flex-col py-25 gap-30">
         <Balance />
         <Accounts />
--- src/app/routes/app/Transactions.tsx
+++ src/app/routes/app/Transactions.tsx
@@ -4,7 +4,7 @@ import { SidebarInset } from "@/components/ui/sidebar";
 function Transactions() {
   return (
     <SidebarInset className="px-5 py-5 flex flex-col min-w-0 hover:cursor-default">
-      <SiteHeader heading="Transactions" name={null} />
+      <SiteHeader heading="Transactions" username={null} />
       <main className="flex-auto flex flex-col py-25 gap-30"></main>
     </SidebarInset>
   );
