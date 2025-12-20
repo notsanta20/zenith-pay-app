396ef87e0c243ec66b1f2f339a033568f1332165
santhosh <santhosh.003z@gmail.com>
2025-12-20 23:52:53 +0530

    added profile content component

--- src/app/routes/app/Profile.tsx
+++ src/app/routes/app/Profile.tsx
@@ -1,3 +1,4 @@
+import ProfileContent from "@/components/custom/Profile/ProfileContent";
 import ProfileHeader from "@/components/custom/Profile/ProfileHeader";
 import SiteHeader from "@/components/custom/SiteHeader";
 import { SidebarInset } from "@/components/ui/sidebar";
@@ -7,7 +8,10 @@ export default function Profile() {
     <SidebarInset className="px-5 py-5 flex flex-col min-w-0 hover:cursor-default">
       <SiteHeader heading="Profile" username={null} />
       <main className="flex-auto flex flex-col py-25 gap-30">
-        <ProfileHeader />
+        <div className="flex flex-col gap-5 min-w-[350px] w-full md:min-w-[400px] md:max-w-[700px] md:mx-auto overflow-hidden">
+          <ProfileHeader />
+          <ProfileContent />
+        </div>
       </main>
     </SidebarInset>
   );
