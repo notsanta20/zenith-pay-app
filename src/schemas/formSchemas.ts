2c9b4aaa7d411c04c1a4bd48d06eac775b184fe7
santhosh <santhosh.003z@gmail.com>
2025-12-22 22:58:52 +0530

    feat: added schema for profile update form and password update

--- src/schemas/formSchemas.ts
+++ src/schemas/formSchemas.ts
@@ -1,4 +1,4 @@
-import type { registerFormType } from "@/types/types";
+import type { registerFormType, updatePasswordFormType } from "@/types/types";
 import * as z from "zod";
 
 export const loginFormSchema = z.object({
@@ -98,3 +98,57 @@ export const transactionFormSchema = z.object({
     .min(3, { error: "account name must be at least 3 characters" })
     .max(30, { error: "account name must not exceed 30 characters" }),
 });
+
+export const userProfileUpdateFormSchema = z.object({
+  fullname: z
+    .string({ error: "enter valid name" })
+    .min(5, { error: "fullname must be at least 5 characters" })
+    .max(30, { error: "fullname must not exceed 30 characters" }),
+  dob: z
+    .date({ error: "select date of birth" })
+    .min(new Date("1920-01-01"), { error: "Too old" })
+    .max(new Date("2007-01-01"), { error: "Must be at least 18 years old" }),
+  phone: z
+    .string({ error: "enter valid phone number" })
+    .length(10, { error: "phone number must be 10 numbers" }),
+  password: z
+    .string({ error: "enter password to update profile" })
+    .min(8, { error: "password must be at least 8 characters" }),
+});
+
+export const updatePassowrdFormSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, { error: "password must be at least 8 characters" })
+      .regex(/[A-Z]/, {
+        error: "password must contain at least 1 uppercase letter",
+      })
+      .regex(/[a-z]/, {
+        error: "password must contain at least 1 lowercase letter",
+      })
+      .regex(/[0-9]/, { error: "password must contain at least 1 number" })
+      .regex(/[^A-Za-z0-9]/, {
+        error: "password must contain at least 1 special character",
+      }),
+    confirmPassword: z
+      .string()
+      .min(8, "password must be at least 8 characters")
+      .regex(/[A-Z]/, {
+        error: "password must contain at least 1 uppercase letter",
+      })
+      .regex(/[a-z]/, {
+        error: "password must contain at least 1 lowercase letter",
+      })
+      .regex(/[0-9]/, { error: "password must contain at least 1 number" })
+      .regex(/[^A-Za-z0-9]/, {
+        error: "password must contain at least 1 special character",
+      }),
+  })
+  .refine(
+    (data: updatePasswordFormType) => data.password === data.confirmPassword,
+    {
+      error: "passwords are not matching",
+      path: ["confirmPassword"],
+    },
+  );
