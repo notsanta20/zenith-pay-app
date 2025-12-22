b50bb26bea77b486d27d9ed97bec4af29b38f47c
santhosh <santhosh.003z@gmail.com>
2025-12-22 22:51:38 +0530

    feat: added update password and update notifications api

--- src/apis/putRequests.ts
+++ src/apis/putRequests.ts
@@ -1,8 +1,29 @@
-import type { userProfileFormType } from "@/types/types";
+import type {
+  updatePasswordFormRequest,
+  userProfileFormType,
+} from "@/types/types";
 import { axiosInstance } from "./apiConfig";
 
 export async function updateUserProfileApi(formData: userProfileFormType) {
   const data = await axiosInstance.put("/profile/update", formData);
+
+  return data;
+}
+
+export async function updatePassApi(formData: updatePasswordFormRequest) {
+  const data = await axiosInstance.put("/auth/update-password", formData);
+
+  return data;
+}
+
+export async function updateSecurityNotification() {
+  const data = await axiosInstance.put("/profile/update-security-notification");
+
+  return data;
+}
+
+export async function updateGeneralNotification() {
+  const data = await axiosInstance.put("/profile/update-general-notification");
 
   return data;
 }
