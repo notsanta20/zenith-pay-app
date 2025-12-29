9dadb6bc4d8d1ec8434be7dbd4639ed854fec361
santhosh <santhosh.003z@gmail.com>
2025-12-30 00:11:39 +0530

    feat: added notification component

--- src/app/routes/app/Notifications.tsx
+++ src/app/routes/app/Notifications.tsx
@@ -1,3 +1,4 @@
+import Notification from "@/components/custom/Notifications/Notification";
 import SiteHeader from "@/components/custom/SiteHeader";
 import { SidebarInset } from "@/components/ui/sidebar";
 
@@ -5,7 +6,9 @@ export default function Notifications() {
   return (
     <SidebarInset className="px-5 py-5 flex flex-col min-w-0 hover:cursor-default">
       <SiteHeader heading="Notifications" username={null} />
-      <main className="flex-auto flex flex-col py-25 gap-30"></main>
+      <main className="flex-auto flex flex-col py-25 gap-30">
+        <Notification />
+      </main>
     </SidebarInset>
   );
 }
