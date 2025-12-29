50943343e158ce36c702534a3ecd6a973788d1e7
santhosh <santhosh.003z@gmail.com>
2025-12-30 00:11:22 +0530

    fix: fix typo

--- src/app/routes/app/Accounts.tsx
+++ src/app/routes/app/Accounts.tsx
@@ -5,7 +5,7 @@ import { SidebarInset } from "@/components/ui/sidebar";
 export default function Accounts() {
   return (
     <SidebarInset className="px-5 py-5 flex flex-col min-w-0 hover:cursor-default h-screen">
-      <SiteHeader heading="Transactions" username={null} />
+      <SiteHeader heading="Accounts" username={null} />
       <main className="flex-1 flex flex-col pt-25 gap-30 min-h-0 overflow-hidden">
         <AllAccounts />
       </main>
